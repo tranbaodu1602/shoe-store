0719276175c9a79769e0db91ded62a3807bbc74e
tranbaodu1602 <devtranbaodu1602@gmail.com>
2023-07-03 14:10:22 +0700

    create project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="bg-black text-red-400 text-4xl font-bold"> aaa Store</h1>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      xl: { max: "1200px" },
+      lg: { max: "991px" },
+      md: { max: "767px" },
+      xsm: { max: "375px" },
+    },
+  },
+  plugins: [],
+};
